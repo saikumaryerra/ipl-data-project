882d8b7cbfeb8847704196f463d908e275877d4b
saikumar <yerra.sai@mountblue.io>
2019-06-15 10:39:45 +0530

    added database funcitons and test function to best_entertainer_for_season.py

--- best_entertainer_for_season.py
+++ best_entertainer_for_season.py
@@ -51,11 +51,37 @@ def best_entertainer_for_season(matches_file_path, deliveries_file_path,current_
     top_entertaining_pct=max(entertainment)
     top_entertainer_index=entertainment.index(top_entertaining_pct)
     top_entertainer=teams_current_season[top_entertainer_index]
-    return entertainment, average_score, boundaries_short_name, short_team_names, top_entertainer
+    return entertainment, average_score, boundaries_short_name, short_team_names, top_entertainer,current_season
+
+def best_entertainer_for_season_from_database(table_name_1='matches',table_name_2='deliveries',season='2015'):
+    con,cur=utilities.database_connect()
+    cur.execute('''select batting_team,(sum(total_runs)/count(distinct match_id)),
+                sum (case when total_runs>3 then 1 else 0 end),sum(total_runs)/count(distinct match_id)*4+sum (case when total_runs>3 then 1 else 0 end)*6 as entertainment
+                from '''+table_name_2+''' inner join '''+table_name_1+''' on '''+table_name_1+'''.id='''+table_name_2+'''.match_id
+                where season= \''''+season+'''\' group by batting_team
+                order by entertainment desc;''')
+    rows=cur.fetchall()
+    con.commit()
+    con.close()
+    entertainment_pct=[]
+    average_score={}
+    boundaries_short_name={}
+    short_team_names=[]
+    for team,avg_score,boundaries,entertainment in rows:
+        entertainment_pct.append(entertainment)
+        k=utilities.short_name(team)
+        short_team_names.append(k)
+        average_score[k]=avg_score
+        boundaries_short_name[k]=boundaries
+    top_entertainer=rows[0][0]
+    plot_best_entertainer_for_season(entertainment_pct, average_score, boundaries_short_name, short_team_names, top_entertainer,season)
+    print(entertainment_pct,average_score,boundaries_short_name,short_team_names,top_entertainer,season)
+    return entertainment_pct,average_score,boundaries_short_name,short_team_names,top_entertainer,season
+
 
 # plotting
-def plot_best_entertainer_for_season(entertainment, average_score, boundaries_short_name, short_team_names, top_entertainer):
-    plt.suptitle(top_entertainer+" is best entertaining team for "+current_season,fontweight="bold")
+def plot_best_entertainer_for_season(entertainment, average_score, boundaries_short_name, short_team_names, top_entertainer,season):
+    plt.suptitle(top_entertainer+" is best entertaining team for "+season,fontweight="bold")
     plt.subplot2grid((2,2),(0,0),rowspan=2)
     plt.pie(entertainment, autopct='%1.1f%%', labels=short_team_names)
     plt.subplot2grid((2,2),(0,1))
@@ -64,10 +90,11 @@ def plot_best_entertainer_for_season(entertainment, average_score, boundaries_sh
     utilities.bar_graph_from_dictionary(boundaries_short_name, 'team', 'boundaries')
     plt.show()
 
-def calculate_and_plot_best_entertainer_for_season(matches_file_path, deliveries_file_path,match_id_current_season):
-    result_1,result_2,result_3,result_4,result_5= best_entertainer_for_season(matches_file_path, deliveries_file_path,current_season)
-    plot_best_entertainer_for_season(result_1,result_2,result_3,result_4,result_5)
+# def calculate_and_plot_best_entertainer_for_season(matches_file_path, deliveries_file_path,match_id_current_season):
+#     result_1,result_2,result_3,result_4,result_5= best_entertainer_for_season(matches_file_path, deliveries_file_path,current_season)
+#     plot_best_entertainer_for_season(result_1,result_2,result_3,result_4,result_5)
 
 if __name__ == '__main__':
-    current_season=input("enter season \n")
-    calculate_and_plot_best_entertainer_for_season('./ipl/matches.csv','./ipl/deliveries.csv',current_season)
+    # current_season=input("enter season \n")
+    # calculate_and_plot_best_entertainer_for_season('./ipl/matches.csv','./ipl/deliveries.csv',current_season)
+    best_entertainer_for_season_from_database('test_matches','test_deliveries','2016')
--- test_best_entertainer_for_season.py
+++ test_best_entertainer_for_season.py
@@ -1,6 +1,6 @@
 import unittest
 
-from best_entertainer_for_season import best_entertainer_for_season
+from best_entertainer_for_season import best_entertainer_for_season,best_entertainer_for_season_from_database
 
 
 class TestBestEntertainerForSeason(unittest.TestCase):
@@ -11,7 +11,7 @@ class TestBestEntertainerForSeason(unittest.TestCase):
                 ([53, 28, 24, 19],
                 {'MI': 13, 'RPS': 7, 'RCB': 8, 'SRH': 4},
                 {'MI': 2, 'RPS': 1, 'RCB': 0, 'SRH': 1},
-                ['MI', 'RPS', 'RCB', 'SRH'],'Mumbai Indians'
+                ['MI', 'RPS', 'RCB', 'SRH'],'Mumbai Indians','2015'
                 )
             ),
             (
@@ -20,12 +20,35 @@ class TestBestEntertainerForSeason(unittest.TestCase):
                 {'GL': 8, 'KXP': 13, 'KKR': 16, 'RPS': 14},
                 {'GL': 1, 'KXP': 2, 'KKR': 3, 'RPS': 2},
                 ['GL', 'KXP', 'KKR', 'RPS'],
-                'Kolkata Knight Riders'
+                'Kolkata Knight Riders','2016'
                 )
             )
         ]
         for args, expected_output in inputs_and_outputs:
             output=best_entertainer_for_season(*args)
+            self.assertEqual(output, expected_output)
+    def test_best_entertainer_for_season_from_database(self):
+        inputs_and_outputs = [
+            (
+                ('test_matches','test_deliveries','2015'),
+                ([64, 34, 32, 22],
+                {'MI': 13, 'RPS': 7, 'RCB': 8, 'SRH': 4} ,
+                {'MI': 2, 'RPS': 1, 'RCB': 0, 'SRH': 1},
+                ['MI', 'RPS', 'RCB', 'SRH'],'Mumbai Indians','2015'
+                )
+            ),
+            (
+                ('test_matches','test_deliveries','2016'),
+                ([82, 68, 64, 38],
+                {'KKR': 16, 'RPS': 14, 'KXP': 13, 'GL': 8},
+                {'KKR': 3, 'RPS': 2, 'KXP': 2, 'GL': 1},
+                ['KKR', 'RPS', 'KXP', 'GL'],
+                'Kolkata Knight Riders','2016'
+                )
+            )
+        ]
+        for args, expected_output in inputs_and_outputs:
+            output=best_entertainer_for_season_from_database(*args)
             self.assertEqual(output, expected_output)
 
 if __name__ == '__main__':
